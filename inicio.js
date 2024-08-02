a79386c50608305d441330704e2bd09234ffc0c9
Victtor Hugo Estevam <estevam.victtor@gmail.com>
2024-08-02 20:45:13 -0300

    começo-carossel

--- inicio.js
+++ inicio.js
@@ -30,20 +30,20 @@ function click(clickando,a) {
 
 
 click(tudo,puxando)
-
+click(int,apertou)
   let contador = 0
     let conta = 1
 function apertou( ){
   
     if( conta > contador ){ 
-        graf.style = 'transform: rotateZ(360deg); border: 10px solid #5D1CE6;border-bottom-color: #1BE539;border-left-color: #E6841C; margin-left:80%;' 
-        rol.style = ' border: 2px solid #5D1CE6;'
-        int.style = 'background-color:#5D1CE6;transition:5s;'
+        graf.classList.add('grafstyle')
+        rol.classList.add('rolstyle')
+        int.classList.add('intstyle')
         contador++
     }else{
-   graf.style = 'transform: rotateZ(0deg);border: 10px solid #E62230;border-top-color: #E6E022 ;border-left-color: #22A1E5;' 
-   rol.style = ' justify-content: left;border: 2px solid #E62230;'
-   int.style = 'background-color:#E62230; transition:5s;'
+   graf.classList.remove('grafstyle')
+   rol.classList.remove('rolstyle')
+   int.classList.remove('intstyle')
         conta++
     }
   
@@ -51,18 +51,15 @@ function apertou( ){
 function puxando(){
      if(conta > contador){
         
- tudo.style = 'position:absolute;z-index:999;width:100%;height:100vh; top:0; left:0; trasition:all 2s;'
-   ul.style = 'position: relative; left:20em;top:1.6vh;trasition:all 2s;'
-   nav.style = 'position: relative; left:21em;top:1.6vh;trasition:all 2s;'
+ tudo.classList.add('tudostyle')
+   ul.classList.add('ulstyle')
+   nav.classList.add('navstyle')
   
     contador++
-
-
-  
     }else{
-        tudo.style='right:-7.5em;background-color: transparent;'
-        ul.style ='position:relative;left: 9em;'
-        nav.style = 'position:relative;left: -1em;'
+      tudo.classList.remove('tudostyle')
+      ul.classList.remove('ulstyle')
+      nav.classList.remove('navstyle')
         conta++
     }
 
